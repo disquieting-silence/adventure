a711f1a45003b82da1446dcb16d33d59dbec0258
dsq <dsq.email>
2015-05-23 19:43:43 +1000

    adding turns

--- World.hs
+++ World.hs
@@ -43,6 +43,20 @@ doMove (w, s@(PlayerState current)) dir =
      dest
 
 
+data Action = Move Direction deriving (Eq, Show)
+
+findAction :: String -> Maybe Action
+findAction "N" = Just (Move North)
+findAction "S" = Just (Move South)
+findAction "E" = Just (Move East)
+findAction "W" = Just (Move West)
+findAction _ = Nothing
+
+getAction :: IO (Maybe Action)
+getAction = fmap findAction getLine
+   
+    
+
 gameWorld = World 
    [
      RoomInfo R1 (RoomDesc "Kitchen") 
@@ -53,17 +67,23 @@ gameWorld = World
 
 type App a = WriterT String (StateT (World, PlayerState) IO) a
 
-playGame :: [Direction] -> App GameOutcome 
-playGame [] = do
-     (world, player) <- get
-     return Lose
-playGame (d:ds) = do
+type TurnsLeft = Int
+
+doAction :: TurnsLeft -> Action -> App GameOutcome
+doAction turns (Move dir) = do
      state <- get
-     _line <- liftIO getLine 
-     let (message, world, ps) = doMove state d
+     let (message, world, ps) = doMove state dir
      tell $ message ++ "\n"
      put (world, ps)
-     playGame ds
+     playGame (turns - 1)
+
+playGame :: TurnsLeft -> App GameOutcome 
+playGame 0 = return Lose
+playGame turns = do
+     -- Read the action from the user input
+     input <- liftIO getAction
+     -- 
+     maybe (playGame (turns - 1)) (doAction turns) input
 
 -- this is sort of running something.
 -- (runStateT $ runWriterT (playGame [South, South])) (gameWorld, (PlayerState R1))
