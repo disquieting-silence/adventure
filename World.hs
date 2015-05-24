c8039fc5dc6edefe1b469cdb0d7d358e2c636d8e
dsq <dsq.email>
2015-05-24 13:58:15 +1000

    added short desc

--- World.hs
+++ World.hs
@@ -8,7 +8,10 @@ data Room = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 deriving (Show, Eq)
 
 newtype RoomDesc = RoomDesc String deriving Show
 
-data RoomInfo = RoomInfo Room RoomDesc deriving Show
+data RoomInfo = RoomInfo {
+  getRoom :: Room,
+  getDesc :: RoomDesc
+} deriving Show
 
 data Direction = North | South | East | West deriving (Show, Eq)
 
@@ -59,10 +62,33 @@ getAction = fmap findAction getLine
 
 gameWorld = World 
    [
-     RoomInfo R1 (RoomDesc "Kitchen") 
+     RoomInfo R1 (RoomDesc "Kitchen"),
+     RoomInfo R2 (RoomDesc "Dining Room"),
+     RoomInfo R3 (RoomDesc "Lounge"),
+     RoomInfo R4 (RoomDesc "Ballroom"),
+     RoomInfo R5 (RoomDesc "Rumpus Room"),
+     RoomInfo R6 (RoomDesc "Hall"),
+     RoomInfo R7 (RoomDesc "Conservatory"),
+     RoomInfo R8 (RoomDesc "Library"),
+     RoomInfo R9 (RoomDesc "Study") 
    ]
    [
-     Transition R1 South R2
+     Transition R1 South R2,
+     Transition R2 North R1,
+     Transition R2 East R5,
+     Transition R2 South R3,
+     Transition R3 East R6,
+     Transition R3 North R2,
+     Transition R4 South R5,
+     Transition R5 North R4,
+     Transition R5 West R2,
+     Transition R6 West R3,
+     Transition R6 East R9,
+     Transition R7 South R8,
+     Transition R8 North R7,
+     Transition R8 South R9,
+     Transition R9 North R8,
+     Transition R9 West R6
    ]
 
 type App a = WriterT String (StateT (World, PlayerState) IO) a
@@ -84,17 +110,22 @@ endTurn turns msg = do
      playGame turns 
 
 
-findDesc :: World -> Room -> Maybe RoomDesc
-findDesc w room = 
+findInfo :: World -> Room -> Maybe RoomInfo
+findInfo w room = 
      let rooms = getRooms w
-         info = find (\(RoomInfo r d) -> r == room) rooms
-     in fmap (\(RoomInfo _ d) -> d) info
+     in find (\(RoomInfo r d) -> r == room) rooms
 
 startTurn :: TurnsLeft -> (World, PlayerState) -> [String]
-startTurn turns (w, (PlayerState current)) = [
-   ("Hello. You have " ++ (show turns) ++ " turn(s) remaining."),
-   "Heelo "
- ]
+startTurn turns (w, (PlayerState current)) = 
+   let info = findInfo w current
+       description = maybe 
+        [ "I do not know where you are." ]
+        (\i -> 
+          let (RoomDesc desc) = getDesc i
+          in [ "You are in the " ++ desc ]
+        )
+        info
+   in [ "You have " ++ (show turns) ++ " turn(s) remaining." ] ++ description ++ [ "What is your move?" ]
 
 
 playGame :: TurnsLeft -> App GameOutcome 
