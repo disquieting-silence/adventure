1e002d53a6b75f98d36fa2b5f4701b37aeccea29
dsq <dsq.email>
2015-05-24 16:59:09 +1000

    slightly more original names of rooms

--- src/Main.hs
+++ src/Main.hs
@@ -13,11 +13,11 @@ gameWorld = World
    [
      RoomInfo R1 (RoomName "Kitchen") (RoomDesc "It is a small room with white walls. There is the faint odour of something rotten."),
      RoomInfo R2 (RoomName "Dining Room") (RoomDesc "The table has been prepared for a banquet of some kind."),
-     RoomInfo R3 (RoomName "Lounge") (RoomDesc "The couch is in the centre of the room."),
+     RoomInfo R3 (RoomName "Living Room") (RoomDesc "The couch is in the centre of the room."),
      RoomInfo R4 (RoomName "Ballroom") (RoomDesc "The room looks abandoned. There must not have been many balls for quite some time."),
      RoomInfo R5 (RoomName "Rumpus Room") (RoomDesc "No-one could have any fun here."),
-     RoomInfo R6 (RoomName "Hall") (RoomDesc "There are portraits all over the walls."),
-     RoomInfo R7 (RoomName "Conservatory") (RoomDesc "The plants seem to be dying."),
+     RoomInfo R6 (RoomName "Foyer") (RoomDesc "There are portraits all over the walls."),
+     RoomInfo R7 (RoomName "Greenhouse") (RoomDesc "The plants seem to be dying."),
      RoomInfo R8 (RoomName "Library") (RoomDesc "It would take a lifetime to read all of these books."),
      RoomInfo R9 (RoomName "Study") (RoomDesc "The room is very quiet.") 
    ]
