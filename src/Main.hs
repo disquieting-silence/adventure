59ef8abb69f2fb015781bebac5af839965ff71cd
dsq <dsq.email>
2015-05-24 15:41:51 +1000

    better descriptions

--- src/Main.hs
+++ src/Main.hs
@@ -10,15 +10,15 @@ import Control.Monad.State
 
 gameWorld = World 
    [
-     RoomInfo R1 (RoomDesc "Kitchen"),
-     RoomInfo R2 (RoomDesc "Dining Room"),
-     RoomInfo R3 (RoomDesc "Lounge"),
-     RoomInfo R4 (RoomDesc "Ballroom"),
-     RoomInfo R5 (RoomDesc "Rumpus Room"),
-     RoomInfo R6 (RoomDesc "Hall"),
-     RoomInfo R7 (RoomDesc "Conservatory"),
-     RoomInfo R8 (RoomDesc "Library"),
-     RoomInfo R9 (RoomDesc "Study") 
+     RoomInfo R1 (RoomName "Kitchen") (RoomDesc "It is a small room with white walls. There is the faint odour of something rotten."),
+     RoomInfo R2 (RoomName "Dining Room") (RoomDesc "The table has been prepared for a banquet of some kind."),
+     RoomInfo R3 (RoomName "Lounge") (RoomDesc "The couch is in the centre of the room."),
+     RoomInfo R4 (RoomName "Ballroom") (RoomDesc "The room looks abandoned. There must not have been many balls for quite some time."),
+     RoomInfo R5 (RoomName "Rumpus Room") (RoomDesc "No-one could have any fun here."),
+     RoomInfo R6 (RoomName "Hall") (RoomDesc "There are portraits all over the walls."),
+     RoomInfo R7 (RoomName "Conservatory") (RoomDesc "The plants seem to be dying."),
+     RoomInfo R8 (RoomName "Library") (RoomDesc "It would take a lifetime to read all of these books."),
+     RoomInfo R9 (RoomName "Study") (RoomDesc "The room is very quiet.") 
    ]
    [
      Transition R1 South R2,
--- src/Rooms.hs
+++ src/Rooms.hs
@@ -5,20 +5,23 @@ import Data.List
 data Room = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 deriving (Show, Eq)
 
 newtype RoomDesc = RoomDesc String deriving Show
+newtype RoomName = RoomName String deriving Show
 
 data RoomInfo = RoomInfo {
   getRoom :: Room,
+  getName :: RoomName,
   getDesc :: RoomDesc
 } deriving Show
 
 
 findInfo :: [RoomInfo] -> Room -> Maybe RoomInfo
-findInfo rooms room = find (\(RoomInfo r d) -> r == room) rooms
+findInfo rooms room = find (\info -> (getRoom info) == room) rooms
 
 getDetailOf :: RoomInfo -> [String]
 getDetailOf info =
-  let (RoomDesc desc) = getDesc info
-  in ["You are in " ++ desc ++ "."]
+  let (RoomName name) = getName info
+      (RoomDesc desc) = getDesc info
+  in ["You are in the " ++ name ++ ". " ++ desc]
 
 getDetail :: [RoomInfo] -> Room -> [String]
 getDetail rooms room =
