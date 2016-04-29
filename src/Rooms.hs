4e8f7a4b3b943f94a98c5674054f3dc93d6a8b29
dsq <dsq.email>
2016-04-30 07:39:59 +1000

    slowly grasping how I can use typeclasses

--- src/Rooms.hs
+++ src/Rooms.hs
@@ -20,13 +20,37 @@ data RoomInfo = RoomInfo {
   getDesc :: RoomDesc
 } deriving Show
 
-class Eq k => DetailedInfo i k where
-  sfindInfo :: [i] -> k -> Maybe i
-  sfindInfo infos key = find (\info -> (sgetKey info) == key) infos
-  sgetKey :: i -> k
-  sgetDetailOf :: i -> [String]
-  sgetDetail :: [i] -> k -> [String]
-  stoString :: i -> [String]
+class Eq raw => GameId raw where
+  gDeref :: raw
+
+class GameInfo i where
+  gGetKey :: GameId k => i -> k
+  gLog :: i -> [String]
+
+
+newtype GameError = GameError String
+
+showError :: GameError -> [String]
+showError _ = [ "dog" ]
+
+gGetDetail :: (GameInfo i, GameId k) => [i] -> k -> GameError -> [String]
+gGetDetail infos key err =
+  let mInfo = find (\i -> (gGetKey i) == key) infos
+  in maybe (showError err) gLog mInfo
+-- class Eq k => DetailedInfo i k e where
+--   sfindInfo :: [i] -> k -> Maybe i
+--   sfindInfo infos key = find (\info -> (sgetKey info) == key) infos
+--   sgetKey :: i -> k
+--   sgetDetailOf :: i -> [String]
+--   sgetDetail :: [i] -> k -> e -> [String]
+--   sgetDetail infos key err =
+--     let info = sfindInfo infos key
+--     in maybe (errMessage err) (\_ -> ["Cat"]) info
+--   -- sgetDetail infos key =
+--   --   let info = sfindInfo infos key
+--   --   in maybe (errMessage key) sgetDetailOf info
+--   stoString :: i -> [String]
+--   errMessage :: e -> [String]
 
 findInfo :: [RoomInfo] -> Room -> Maybe RoomInfo
 findInfo rooms room = find (\info -> (getRoom info) == room) rooms
